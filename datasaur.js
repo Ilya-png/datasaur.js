e9abecb457320afc0c7de6f74ec57c0f50838be5
Ilyas Jibril <jibriily2@gmail.com>
2020-10-26 22:15:01 -0400

    datasaur.js

---
+++ datasaur.js
@@ -0,0 +1,160 @@
+
+
+const makeDino = function (name){
+  return name. && diet;
+}
+
+const expectedDino = function(name){
+ const expectedDino =[]
+ for (const name of name)
+ if (ismakeDino(name)){
+   result.push(name)
+ }
+   
+ }
+
+ return dinosourse
+ 
+}
+
+
+
+const makeSingular =function(nam){
+   
+}
+
+const truncateSpecies=function (params) {
+return dinosaur.length<=10;
+
+const dinosourse =[]
+for (const str of strs){
+  fixedStrs.push()
+  ()
+}
+return dinosaur;
+
+}
+
+
+const isCornivore=function(params) {
+if ( is dinosour)=true;
+else()false;
+}
+
+const isExtinct = function name(params) {
+  if (the dinosaur is extingt)=true;
+  else()=false;  
+}
+
+const istriassic= function (params) {
+  if ( is Triassic )=false;
+  else()=true;
+  
+}
+
+
+const is=function(params) {
+  if ( is dinosour)=true;
+  else()false;
+  }
+  
+  const isExtinct = function name(params) {
+    if (the dinosaur is extingt)=true;
+    else()=false;  
+  }
+  
+  const istriassic= function (params) {
+    if ( is Triassic )=false;
+    else()=true;
+    
+  }
+
+
+  const isCornivore=function(params) {
+    if ( is dinosour)=true;
+    else()false;
+    }
+    
+    const isExtinct = function name(params) {
+      if (the dinosaur is extingt)=true;
+      else()=false;  
+    }
+    
+    const istriassic= function (params) {
+      if ( is Triassic )=false;
+      else()=true;
+      
+    }
+
+
+
+    const isCornivore=function(params) {
+      if ( is dinosour)=true;
+      else()false;
+      }
+      
+      const isExtinct = function name(params) {
+        if (the dinosaur is extingt)=true;
+        else()=false;  
+      }
+      
+      const istriassic= function (params) {
+        if ( is Triassic )=false;
+        else()=true;
+        
+      }
+
+      const isCornivore=function(params) {
+        if ( is dinosour)=true;
+        else()false;
+        }
+        
+        const isExtinct = function name(params) {
+          if (the dinosaur is extingt)=true;
+          else()=false;  
+        }
+        
+        const istriassic= function (params) {
+          if ( is Triassic )=false;
+          else()=true;
+          
+        }
+
+
+        const isCornivore=function(params) {
+          if ( is dinosour)=true;
+          else()=false;
+          }
+          
+          const isExtinct = function name(params) {
+            if (the dinosaur is extingt)=true;
+            else()=false;  
+          }
+          
+          const isJuiassic= function (params) {
+            if ( is Triassic )=false;
+            else()=true;
+            
+      }
+
+     
+
+
+      const isCretaceous= function (params) {
+        if ( is dinosaur is from the Cretaceous)=true;
+        else()false;
+
+        }
+        
+      }
+
+
+
+
+
+
+
+
+
+
+
